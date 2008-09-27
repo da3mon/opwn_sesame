47f48ef8bbb62cf6710cfc143524de61ef990b98
da3mon mccormick <da3mon@0xdeadcode.org>
2008-09-27 14:27:11 -0400

    cleaning up links. need to move views back and rename them.

--- opwn_sesame.rb
+++ opwn_sesame.rb
@@ -1,4 +1,4 @@
-%w(rubygems sinatra dm-core).each { |f| require f }
+%w(rubygems sinatra dm-core dm-validations).each { |f| require f }
 
 class EntrySystem
   include DataMapper::Resource
@@ -15,11 +15,19 @@ end
 class Image
   include DataMapper::Resource
   property :id, Serial, :key => true
+  property :filename, String, :nullable => false
+  property :content_type, String, :nullable => false
+  
+  belongs_to :entry_system
 end
 
 class PdfManual
   include DataMapper::Resource
   property :id, Serial, :key => true
+  property :filename, String, :nullable => false
+  property :content_type, String, :nullable => false
+  
+  belongs_to :entry_system
 end
 
 configure do
@@ -31,35 +39,54 @@ layout 'layout.erb'
 
 get '/' do
   @entry_systems = EntrySystem.all
-  erb :index, :views_directory => File.dirname(__FILE__) + "/views/entry_systems"
+  erb :index
 end
 
 get '/entry_systems/new' do
   @entry_system = EntrySystem.new
-  erb :new, :views_directory => File.dirname(__FILE__) + "/views/entry_systems"
+  erb :new
 end
 
 get '/entry_systems/:id' do
-  @entry_system = EntrySystem.get!(params[:id])
-  erb :show, :views_directory => File.dirname(__FILE__) + "/views/entry_systems"
+  @entry_system = EntrySystem.get(params[:id])
+  erb :show
+end
+
+get '/entry_systems/:id/edit' do
+  @entry_system = EntrySystem.get(params[:id])
+  erb :edit
 end
 
 post '/entry_systems' do
-  @entry_system = EntrySystem.create!(
+  @entry_system = EntrySystem.new(
     :name => params[:name],
     :manufacturer => params[:manufacturer],
     :prompt => params[:prompt],
     :password => params[:password]
   )
-  redirect "/"
+  if @entry_system.save
+    redirect "/"
+  else
+    @errors = @entry_system.errors.full_messages.join("<br />")
+    erb :new
+  end
 end
 
 put '/entry_systems/:id' do
-  @entry_system = EntrySystem.find(:id).update_attributes(
+  @entry_system = EntrySystem.get(params[:id])
+  if @entry_system.update_attributes(
     :name => params[:name],
     :manufacturer => params[:manufacturer],
     :prompt => params[:prompt],
     :password => params[:password]
   )
-  redirect "/entry_systems/#{@entry_system.id}"
+    redirect "/entry_systems/#{@entry_system.id}"
+  else
+    @errors = @entry_system.errors.full_messages.join("<br />")
+    erb :edit
+  end
+end
+
+delete '/entry_systems/:id' do
+  EntrySystem.get(params[:id]).destroy
 end
--- spec/opwn_sesame_spec.rb
+++ spec/opwn_sesame_spec.rb
@@ -2,15 +2,60 @@
 
 describe "opwn_sesame" do
   require "opwn_sesame"
-  attr_reader :response
+  attr_reader :response, :ivars
+  before(:each) do
+    EntrySystem.all.should == []
+  end
+  
+  after(:each) do
+    EntrySystem.all.destroy!
+  end
   
   it "/ gets a list of entry systems" do
+    es = EntrySystem.create(:name => "name", :manufacturer => "manufacturer")
+    
     get_it '/'
     response.should be_ok
+    body.should include(es.name)
+    body.should include(es.manufacturer)
   end
   
   it "gets /entry_systems/new" do
     get_it '/entry_systems/new'
     response.should be_ok
+    body.should =~ /method="post"/
+    body.should =~ /action="\/entry_systems"/
+  end
+  
+  describe "entry_system creation" do
+    attr_reader :params
+    before(:each) do
+      @params = {:name => "name", :manufacturer => "manufacturer"}
+    end
+
+    context "with valid params" do
+      it "creates the entry_system and redirects" do      
+        post_it "/entry_systems", :name => params[:name], :manufacturer => params[:manufacturer]
+
+        p response
+        p body
+
+        response.should be_redirection
+        follow!
+
+        body.should include("name")
+        body.should include("manufacturer")
+      end
+    end
+    
+    context "with invalid params" do
+      it "creates entry systems" do      
+        post_it "/entry_systems", :manufacturer => params[:manufacturer]
+        response.should be_ok
+
+        body.should_not include("name")
+        body.should_not include("manufacturer")
+      end
+    end
   end
 end
